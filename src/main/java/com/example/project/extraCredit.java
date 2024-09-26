2687f05c098007546a4a14d18718f60d6ec6455e
jt2738 <jaheent@nycstudents.net>
2024-09-26 12:40:51 +0000

    second commit

--- src/main/java/com/example/project/extraCredit.java
+++ src/main/java/com/example/project/extraCredit.java
@@ -1,27 +1,39 @@
 package com.example.project;
 
-public class TipCalculator {
-    //WRITE YOUR PROGRAM IN calculateTip
+import java.util.Scanner ;
+public class extraCredit {
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
         
         // calc tip and bill
         double tip = (cost * ((double)percent/100));
+        double x = tip ;
         // tip = Math.round(x * 100.0) / 100.0; // I got the rounding from here: https://stackoverflow.com/questions/8825209/rounding-decimal-points
         double billTip = cost + tip;  
+        
         // calc person per cost
         double personPerCost = cost / people ;
+        double z = personPerCost ;
+        
+        
         // calc tip per person
         double tipPerPerson = tip/ people ;
+        double a = tipPerPerson ;
+        // tipPerPerson = Math.round(a * 100.0) / 100.0; 
+
         // calc total cost per person 
         double totalPerPerson = billTip / people ;
+        double b = totalPerPerson ;
         
-        tip = Math.round(tip * 100.0) / 100.0;
-        billTip = Math.round(billTip * 100.0) / 100.0;
-        personPerCost = Math.round(personPerCost * 100.0) / 100.0; 
-        tipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0; 
-        totalPerPerson = Math.round(totalPerPerson * 100.0)/100.0;
+        tip = Math.round(x * 100.0) / 100.0;
+        totalPerPerson = Math.round(b*100.0)/100.0;
+        // tip = Math.round(x * 100.0) / 100.0 ; 
+        tipPerPerson = Math.round(a * 100.0) / 100.0; 
+        personPerCost = Math.round(z * 100.0) / 100.0; 
+        billTip = Math.round(billTip* 100.0) / 100.0;
+        
+
                        
         // outputs 
         result.append("-------------------------------\n");
@@ -33,17 +45,32 @@ public class TipCalculator {
         result.append("Tip per person: $" + tipPerPerson + "\n");
         result.append("Total cost per person: $" +  totalPerPerson + "\n");
         result.append("-------------------------------\n");
-
-
+        // System.out.println("Items ordered: \n" + Items  );
         return result.toString();
     }
+
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
+        Scanner scan = new Scanner(System.in); //ec starts here
+        String userAns = "x";
+        String character = "-1";
+        String Items= " ";
+        while(!userAns.equals(character)) {
+            System.out.println("Enter an Item or type -1 to finish");
+            userAns = scan.nextLine();
+            if (!userAns.equals(character)) {
+                Items += userAns + "\n" ;
+            }
+        }
         int people = 12;
         int percent = 15;
         double cost = 566.97;
         System.out.println(calculateTip(people,percent,cost));
+        System.out.println("Items ordered: \n" + Items  ); // output of items
+        scan.close();
     }
 }
         
+
+
